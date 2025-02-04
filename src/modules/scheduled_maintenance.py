6fe95f5471e559bf132b584d553456e7d92f089d
Shaquan Robinson <56005808+Shaq44@users.noreply.github.com>
2025-02-04 11:48:38 -0800

    Updated

--- src/modules/scheduled_maintenance.py
+++ src/modules/scheduled_maintenance.py
@@ -20,26 +20,41 @@ def cleanUpDir(temp_dir):
 
 
 
-def checkDuplicateFile(file,temp_dir):
+def showDirectory():
+    temp_dir=input("Enter directory you would like to see: ")
     for files in os.listdir(temp_dir):
-        if file == files:
-            print(file)
-            os.remove(file) 
-        #print("These are the files in: \b {}".format(temp_dir))
-        #if os.path.isfile(file):
-         #   print("{} is a file in a directory!".format(file))
-          #  print( os.path.getsize(file))
-           # return file
-        #else:
-         #   print("This file does not exist")
+        print(files)
+            
+def deleteFile():
+    file=input("Enter file you wish to delete: ")
+    if os.path.isfile(file):
+        os.remove(file)
+        print("File was deleted")
+    else:
+        print("This is not a file")    
+
 
 
 #temp_dir=input("Enter directory you are looking for: ")
 #cleanUpDir(temp_dir)        
-
-temp_dir=input("Enter path to directory and file name you are looking for: ")
-file=input("Enter file you are looking for: ")
-checkDuplicateFile(file,temp_dir)
     
+#file = input("Enter file you wish delete: ")
+#deleteFile(file)
+
+#temp_dir= input("Enter directory you would like to see: ")
+#showDirectory(temp_dir)
+
+def menu():
+    choice_entry= input("Menu:\n 1:Show contents of directory\n 2:Delete a file \n Enter menu option:")
+    match choice_entry:
+        case "1": 
+           return showDirectory()
+        case "2":
+            return deleteFile()
+        case _:
+            return "The Program is closing"
 
 
+
+#choice = input("Enter your menu option:\n 1- Show contents of directory\n 2- Delete a file" )
+menu()
